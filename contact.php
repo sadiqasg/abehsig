234f56cf12caf44afc3c8f25cdf15668bd147ee0
saslamp <sadiqasg@gmail.com>
2021-07-06 07:56:59 +0100

    updates & bugfixes

--- contact.php
+++ contact.php
@@ -1,12 +1,21 @@
 <?php
 
-// if ($_POST && isset($_POST['submit']) && isset($_POST['name']) && isset($_POST['email']) && !empty($_POST['name']) && !empty($_POST['email'])) {
 if($_POST["message"]) {
   $name = $_POST["username"];
   $email = $_POST["email"];
-  $to = "sadiqasg@gmail.com";
-  $subject = "Contacting From Abeh Signature";
-  $headers = "From: $email\n";
+  $to = "Abubakar Sambo <sadiqasg@gmail.com>";
+  $subject = "$name via Abeh Signature";
+
+  // $headers = "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n";
+  $headers .= "Reply-To: $name <$email>\r\n"; 
+  $headers .= "Return-Path: $name <$email>\r\n"; 
+  $headers .= "From: $name <$email>\r\n";  
+  $headers .= "Organization: Abeh Signature\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "X-Priority: 3\r\n";
+  $headers .= "X-Mailer: PHP/". phpversion() ."\r\n" ;
+
   $message = $_POST["message"];
   
   if(!mail($to,$subject,$message,$headers)) {
